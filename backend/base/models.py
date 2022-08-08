b93189a7dbd1f359299c1e48cae51dfafa5bffff
Lioncat2002 <sankhayan2002@gmail.com>
2022-08-08 12:09:58 +0530

    chore: switching db to mongodb

--- backend/base/models.py
+++ backend/base/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class ClubMember(models.Model):
-    name=models.CharField(max_length=200)
-    phone=models.CharField(max_length=10)
-    attendence=models.IntegerField()
-    is_admin=models.BooleanField(default=False)
+    name=models.CharField(max_length=200,null=False,blank=False)
+    phone=models.CharField(max_length=10,null=False,blank=False)
+    attendence=models.IntegerField(null=False,blank=False)
+    is_admin=models.IntegerField(default=False)
