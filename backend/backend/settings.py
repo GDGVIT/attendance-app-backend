7b5f103ac4c804be0934e67306ee9a9356340450
Lioncat2002 <sankhayan2002@gmail.com>
2022-08-08 23:02:35 +0530

    chore: added swaggerui and getuser endpoint

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -124,7 +124,7 @@ STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
-
+REST_FRAMEWORK = { 'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema' }
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Configure Django App for Heroku.
--- backend/base/views.py
+++ backend/base/views.py
@@ -1,8 +1,24 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets,status
+from rest_framework.response import Response
+from rest_framework.decorators import api_view,action
 # Create your views here.
 from .import models
 from . import serializers
 class ClubMemberViewSet(viewsets.ModelViewSet):
     queryset=models.ClubMember.objects.all().order_by('id')
     serializer_class=serializers.ClubMemberSerializer
+
+@api_view(['GET'])
+def get_user(request,phno:int):
+    try:
+        user=models.ClubMember.objects.get(phone=phno)
+        s=serializers.ClubMemberSerializer(user)
+        return Response(s.data)
+    except models.ClubMember.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+#@api_view(['PUT'])
+
+
+    
