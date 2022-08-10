5e22007fa2c0e5bf79ac553463138a31fd9ed718
Lioncat2002 <sankhayan2002@gmail.com>
2022-08-10 14:38:09 +0530

    refactor: fix formatting

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv("KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS:list[str] = []
+ALLOWED_HOSTS: list[str] = []
 
 
 # Application definition
