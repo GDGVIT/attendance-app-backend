7e91e10f20a533b94a4d7b0ea1e3945fa8fc44cd
anirudh <anirudh04mishra@gmail.com>
2023-10-11 18:07:54 +0530

    feat: init and dockerise

---
+++ migrations/migration.go
@@ -0,0 +1,16 @@
+package migrations
+
+import (
+	"github.com/GDGVIT/attendance-app-backend/infra/database"
+	"github.com/GDGVIT/attendance-app-backend/models"
+)
+
+// Migrate Add list of model add for migrations
+// TODO later separate migration each models
+func Migrate() {
+	var migrationModels = []interface{}{&models.Example{}}
+	err := database.DB.AutoMigrate(migrationModels...)
+	if err != nil {
+		return
+	}
+}
