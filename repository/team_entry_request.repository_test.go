b21f769e10af27dfe07a4bcb3e6fed3438370513
anirudh <anirudh04mishra@gmail.com>
2023-10-17 23:00:17 +0530

    test(team): :white_check_mark: add missing team member and entry tests

--- repository/team_entry_request.repository_test.go
+++ repository/team_entry_request.repository_test.go
@@ -92,3 +92,93 @@ func TestTeamEntryRequestRepository_UpdateTeamEntryRequestStatus(t *testing.T) {
 		t.Errorf("Expected status to be %s, but got %s", models.TeamEntryRequestApproved, updatedRequest.Status)
 	}
 }
+
+func TestTeamEntryRequestRepository_GetTeamEntryRequestsByUserID(t *testing.T) {
+	db, err := test_utils.SetupTestDB()
+	if err != nil {
+		t.Fatalf("Failed to set up the test database: %v", err)
+	}
+	defer db.Migrator().DropTable(&models.TeamEntryRequest{})
+
+	repository := NewTeamEntryRequestRepository()
+	repository.db = db
+
+	requests := []models.TeamEntryRequest{
+		{TeamID: 1, UserID: 1},
+		{TeamID: 1, UserID: 2},
+		{TeamID: 2, UserID: 3},
+	}
+
+	for _, request := range requests {
+		repository.CreateTeamEntryRequest(request)
+	}
+
+	retrievedRequests, err := repository.GetTeamEntryRequestsByUserID(1)
+	if err != nil {
+		t.Errorf("Expected no error, but got: %v", err)
+	}
+
+	if len(retrievedRequests) != 1 {
+		t.Errorf("Expected 1 request, but got %d", len(retrievedRequests))
+	}
+}
+
+func TestTeamEntryRequestRepository_GetTeamEntryRequestsByUserIDAndStatus(t *testing.T) {
+	db, err := test_utils.SetupTestDB()
+	if err != nil {
+		t.Fatalf("Failed to set up the test database: %v", err)
+	}
+	defer db.Migrator().DropTable(&models.TeamEntryRequest{})
+
+	repository := NewTeamEntryRequestRepository()
+	repository.db = db
+
+	requests := []models.TeamEntryRequest{
+		{TeamID: 1, UserID: 1, Status: models.TeamEntryRequestApproved},
+		{TeamID: 1, UserID: 2, Status: models.TeamEntryRequestPending},
+		{TeamID: 2, UserID: 3, Status: models.TeamEntryRequestApproved},
+	}
+
+	for _, request := range requests {
+		repository.CreateTeamEntryRequest(request)
+	}
+
+	retrievedRequests, err := repository.GetTeamEntryRequestsByUserIDAndStatus(1, models.TeamEntryRequestApproved)
+	if err != nil {
+		t.Errorf("Expected no error, but got: %v", err)
+	}
+
+	if len(retrievedRequests) != 1 {
+		t.Errorf("Expected 1 request, but got %d", len(retrievedRequests))
+	}
+}
+
+func TestTeamEntryRequestRepository_GetTeamEntryRequestsByTeamIDAndStatus(t *testing.T) {
+	db, err := test_utils.SetupTestDB()
+	if err != nil {
+		t.Fatalf("Failed to set up the test database: %v", err)
+	}
+	defer db.Migrator().DropTable(&models.TeamEntryRequest{})
+
+	repository := NewTeamEntryRequestRepository()
+	repository.db = db
+
+	requests := []models.TeamEntryRequest{
+		{TeamID: 1, UserID: 1, Status: models.TeamEntryRequestApproved},
+		{TeamID: 1, UserID: 2, Status: models.TeamEntryRequestPending},
+		{TeamID: 2, UserID: 3, Status: models.TeamEntryRequestApproved},
+	}
+
+	for _, request := range requests {
+		repository.CreateTeamEntryRequest(request)
+	}
+
+	retrievedRequests, err := repository.GetTeamEntryRequestsByTeamIDAndStatus(1, models.TeamEntryRequestApproved)
+	if err != nil {
+		t.Errorf("Expected no error, but got: %v", err)
+	}
+
+	if len(retrievedRequests) != 1 {
+		t.Errorf("Expected 1 request, but got %d", len(retrievedRequests))
+	}
+}
--- repository/team_member.repository_test.go
+++ repository/team_member.repository_test.go
@@ -181,3 +181,209 @@ func TestTeamMemberRepository_GetTeamMembersByTeamID(t *testing.T) {
 		// Add more comparisons for other fields as needed
 	}
 }
+
+func TestTeamMemberRepository_GetTeamMembersByUserID(t *testing.T) {
+	db, err := test_utils.SetupTestDB()
+	if err != nil {
+		t.Fatalf("Failed to set up the test database: %v", err)
+	}
+	defer db.Migrator().DropTable(&models.TeamMember{})
+
+	// Create the TeamMember Repository with the test database
+	tmr := NewTeamMemberRepository()
+	tmr.db = db
+
+	// Create test team members with the same UserID
+	userID := uint(1)
+	teamMembers := []models.TeamMember{
+		{TeamID: 1, UserID: userID, Role: models.MemberRole},
+		{TeamID: 2, UserID: userID, Role: models.AdminRole},
+		{TeamID: 3, UserID: userID, Role: models.MemberRole},
+	}
+
+	for _, teamMember := range teamMembers {
+		_, err = tmr.CreateTeamMember(teamMember)
+		if err != nil {
+			t.Fatalf("Failed to create a test team member: %v", err)
+		}
+	}
+
+	// Test GetTeamMembersByUserID function to retrieve team members by UserID
+	retrievedTeamMembers, err := tmr.GetTeamMembersByUserID(userID)
+	if err != nil {
+		t.Errorf("GetTeamMembersByUserID returned an error: %v", err)
+	}
+
+	// Check the number of retrieved team members
+	expectedCount := len(teamMembers)
+	if len(retrievedTeamMembers) != expectedCount {
+		t.Errorf("Expected %d team members, got %d", expectedCount, len(retrievedTeamMembers))
+	}
+
+	// Verify the correctness of the retrieved team members
+	for i, retrieved := range retrievedTeamMembers {
+		if retrieved.TeamID != teamMembers[i].TeamID {
+			t.Errorf("Mismatch in TeamID. Expected: %d, Got: %d", teamMembers[i].TeamID, retrieved.TeamID)
+		}
+		if retrieved.Role != teamMembers[i].Role {
+			t.Errorf("Mismatch in Role. Expected: %s, Got: %s", teamMembers[i].Role, retrieved.Role)
+		}
+		// Add more comparisons for other fields as needed
+	}
+}
+
+func TestTeamMemberRepository_GetTeamMembersByUserAndRole(t *testing.T) {
+	db, err := test_utils.SetupTestDB()
+	if err != nil {
+		t.Fatalf("Failed to set up the test database: %v", err)
+	}
+	defer db.Migrator().DropTable(&models.TeamMember{})
+
+	// Create the TeamMember Repository with the test database
+	tmr := NewTeamMemberRepository()
+	tmr.db = db
+
+	// Create test team members with the same UserID and Role
+	userID := uint(1)
+	role := models.MemberRole
+	teamMembers := []models.TeamMember{
+		{TeamID: 1, UserID: userID, Role: role},
+		{TeamID: 2, UserID: userID, Role: role},
+		{TeamID: 3, UserID: userID, Role: role},
+	}
+
+	for _, teamMember := range teamMembers {
+		_, err = tmr.CreateTeamMember(teamMember)
+		if err != nil {
+			t.Fatalf("Failed to create a test team member: %v", err)
+		}
+	}
+
+	// Test GetTeamMembersByUserAndRole function to retrieve team members by UserID and Role
+	retrievedTeamMembers, err := tmr.GetTeamMembersByUserAndRole(userID, role)
+	if err != nil {
+		t.Errorf("GetTeamMembersByUserAndRole returned an error: %v", err)
+	}
+
+	// Check the number of retrieved team members
+	expectedCount := len(teamMembers)
+	if len(retrievedTeamMembers) != expectedCount {
+		t.Errorf("Expected %d team members, got %d", expectedCount, len(retrievedTeamMembers))
+	}
+
+	// Verify the correctness of the retrieved team members
+	for i, retrieved := range retrievedTeamMembers {
+		if retrieved.TeamID != teamMembers[i].TeamID {
+			t.Errorf("Mismatch in TeamID. Expected: %d, Got: %d", teamMembers[i].TeamID, retrieved.TeamID)
+		}
+		if retrieved.UserID != teamMembers[i].UserID {
+			t.Errorf("Mismatch in UserID. Expected: %d, Got: %d", teamMembers[i].UserID, retrieved.UserID)
+		}
+		// Add more comparisons for other fields as needed
+	}
+}
+
+func TestTeamMemberRepository_GetTeamMembersByTeamAndRole(t *testing.T) {
+	db, err := test_utils.SetupTestDB()
+	if err != nil {
+		t.Fatalf("Failed to set up the test database: %v", err)
+	}
+	defer db.Migrator().DropTable(&models.TeamMember{})
+
+	// Create the TeamMember Repository with the test database
+	tmr := NewTeamMemberRepository()
+	tmr.db = db
+
+	// Create test team members with the same TeamID and Role
+	teamID := uint(1)
+	role := models.MemberRole
+	teamMembers := []models.TeamMember{
+		{TeamID: teamID, UserID: 1, Role: role},
+		{TeamID: teamID, UserID: 2, Role: role},
+		{TeamID: teamID, UserID: 3, Role: role},
+		{TeamID: teamID, UserID: 4, Role: models.AdminRole},
+	}
+
+	for _, teamMember := range teamMembers {
+		_, err = tmr.CreateTeamMember(teamMember)
+		if err != nil {
+			t.Fatalf("Failed to create a test team member: %v", err)
+		}
+	}
+
+	// Test GetTeamMembersByTeamAndRole function to retrieve team members by TeamID and Role
+	retrievedTeamMembers, err := tmr.GetTeamMembersByTeamAndRole(teamID, role)
+	if err != nil {
+		t.Errorf("GetTeamMembersByTeamAndRole returned an error: %v", err)
+	}
+
+	// Check the number of retrieved team members
+	expectedCount := 3
+	if len(retrievedTeamMembers) != expectedCount {
+		t.Errorf("Expected %d team members, got %d", expectedCount, len(retrievedTeamMembers))
+	}
+
+	// Verify the correctness of the retrieved team members
+	for i, retrieved := range retrievedTeamMembers {
+		if retrieved.UserID != teamMembers[i].UserID {
+			t.Errorf("Mismatch in UserID. Expected: %d, Got: %d", teamMembers[i].UserID, retrieved.UserID)
+		}
+		if retrieved.Role != teamMembers[i].Role {
+			t.Errorf("Mismatch in Role. Expected: %s, Got: %s", teamMembers[i].Role, retrieved.Role)
+		}
+		// Add more comparisons for other fields as needed
+	}
+}
+
+// test GetAdminTeamByTeamID, add both admin and non admin users to test. test this function
+func TestTeamMemberRepository_GetAdminTeamByTeamID(t *testing.T) {
+	db, err := test_utils.SetupTestDB()
+	if err != nil {
+		t.Fatalf("Failed to set up the test database: %v", err)
+	}
+	defer db.Migrator().DropTable(&models.TeamMember{})
+
+	// Create the TeamMember Repository with the test database
+	tmr := NewTeamMemberRepository()
+	tmr.db = db
+
+	// Create test team members with the same TeamID and Role
+	teamID := uint(1)
+	role := models.AdminRole
+	teamMembers := []models.TeamMember{
+		{TeamID: teamID, UserID: 1, Role: role},
+		{TeamID: teamID, UserID: 2, Role: role},
+		{TeamID: teamID, UserID: 3, Role: role},
+		{TeamID: teamID, UserID: 4, Role: models.MemberRole},
+	}
+
+	for _, teamMember := range teamMembers {
+		_, err = tmr.CreateTeamMember(teamMember)
+		if err != nil {
+			t.Fatalf("Failed to create a test team member: %v", err)
+		}
+	}
+
+	// Test GetTeamMembersByTeamAndRole function to retrieve team members by TeamID and Role
+	retrievedTeamMembers, err := tmr.GetAdminTeamByTeamID(teamID)
+	if err != nil {
+		t.Errorf("GetTeamMembersByTeamAndRole returned an error: %v", err)
+	}
+
+	// Check the number of retrieved team members
+	expectedCount := 3
+	if len(retrievedTeamMembers) != expectedCount {
+		t.Errorf("Expected %d team members, got %d", expectedCount, len(retrievedTeamMembers))
+	}
+
+	// Verify the correctness of the retrieved team members
+	for i, retrieved := range retrievedTeamMembers {
+		if retrieved.UserID != teamMembers[i].UserID {
+			t.Errorf("Mismatch in UserID. Expected: %d, Got: %d", teamMembers[i].UserID, retrieved.UserID)
+		}
+		if retrieved.Role != teamMembers[i].Role {
+			t.Errorf("Mismatch in Role. Expected: %s, Got: %s", teamMembers[i].Role, retrieved.Role)
+		}
+		// Add more comparisons for other fields as needed
+	}
+}
